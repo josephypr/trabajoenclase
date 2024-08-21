06a66572662f1dee4c54f32903feb53fef896337
FabianAmaya8 <ozkaritogiofabis@gmail.com>
2024-08-21 13:16:24 -0500

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+menu= int (input("que quiere hacer? 1 jugar // 2 calcular // 3 salir"))
+
+if menu == 1:
+    print("jugando")
+    
+elif menu == 2:
+    print("calculando")
+
+else:
+    print("saliendo")
